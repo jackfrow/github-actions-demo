6f3f98fb812017cd8c7d6d8a405795d724e124e2
CD_WZL <>
2024-11-08 16:19:35 +0800

    test action

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	log "github.com/sirupsen/logrus"
+)
+
+func main() {
+
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.String(200, "hello actions")
+	})
+	r.GET("/hello", func(c *gin.Context) {
+		log.Print("hello request")
+
+		c.JSON(200, gin.H{
+			"code":  200,
+			"state": "success",
+			"data":  "hello word",
+		})
+	})
+
+	r.Run(":9090")
+
+}
